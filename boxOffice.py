32121b4fd1ce6c12ccc8ef8ed7e49013bde475e1
mossi <mossi.hashemi@gmail.com>
2019-10-14 13:11:32 +0200

    open and write on a csv file

---
+++ boxOffice.py
@@ -0,0 +1,6 @@
+csv = open( "boxx.csv","a")
+csv.write("sep=#\n") #define csv separator character 
+csv.write("Number of the article#Number of appearance of the paragrapg in the article#Number of paragraphs of the article#Url of the webpage of the article#Category of the subsection#Author#Year#Month#Day#Hour#Title of the article#Word Count#Text of the paragraph\n")
+    
+
+csv.close()
